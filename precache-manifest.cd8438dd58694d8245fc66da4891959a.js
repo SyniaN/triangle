4896a184034dd2e1584b58c34496b8b9cf99770d
David Xu <davidxu1991@gmail.com>
2019-02-11 22:16:13 +1300

    Updates

---
+++ precache-manifest.cd8438dd58694d8245fc66da4891959a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a3047cb81c48765c9f6",
+    "url": "/triangle/static/js/runtime~main.4a3047cb.js"
+  },
+  {
+    "revision": "32ea21346ec02ca2c544",
+    "url": "/triangle/static/js/main.32ea2134.chunk.js"
+  },
+  {
+    "revision": "09aed32ec1dc4f93facd",
+    "url": "/triangle/static/js/1.09aed32e.chunk.js"
+  },
+  {
+    "revision": "ee91c8a1462e900e8b3816347e4e4be5",
+    "url": "/triangle/index.html"
+  }
+];
